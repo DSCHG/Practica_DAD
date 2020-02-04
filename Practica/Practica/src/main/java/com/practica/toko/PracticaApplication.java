b3f849ea234d77b14ebd8431cd8c285ffe809eee
Darwin <ds.chumapi.2017@alumnos.urjc.es>
2020-02-04 14:07:02 +0100

    proyecto vacio

--- Practica/Practica/src/main/java/com/practica/toko/PracticaApplication.java
+++ Practica/Practica/src/main/java/com/practica/toko/PracticaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Prueba1Application {
+public class PracticaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Prueba1Application.class, args);
+		SpringApplication.run(PracticaApplication.class, args);
 	}
 
 }
