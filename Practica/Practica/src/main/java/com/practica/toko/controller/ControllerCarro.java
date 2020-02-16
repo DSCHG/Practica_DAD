4853f37696dfcc16c4babe3069c94deff81e7c08
Darwin <ds.chumapi.2017@alumnos.urjc.es>
2020-02-16 14:30:58 +0100

    creacion de la vista del producto y la de carro
    con vista de producto me refiero a ver un producto y sus caracteristicas

--- Practica/Practica/src/main/java/com/practica/toko/controller/ControllerCarro.java
+++ Practica/Practica/src/main/java/com/practica/toko/controller/ControllerCarro.java
@@ -1,14 +1,38 @@
 package com.practica.toko.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.practica.toko.model.Carro;
+import com.practica.toko.repositorios.CarroRepository;
 
 @Controller
 public class ControllerCarro {
+	
+	@Autowired
+	private CarroRepository carrodao;
 
 	@RequestMapping("/carro")
-	public String mostrarCarro(Model model) {
-		return "carro";
+	public String mostrarCarro(Model model) {	
+		return "Carrito";
+	}
+	
+	@RequestMapping("/formalizarPedido")
+	public String formalizarPedido() {
+		return "Carrito";
+	}
+	@RequestMapping("/borrarArticulo")
+	public String borrarItem(Model model,@RequestParam(name = "id") String id) {
+		System.out.println(id);
+		return "Carrito";
+	}
+	
+	@RequestMapping("/verProducto")
+	public String verProducto() {
+		return "vistaProducto";
 	}
 }
