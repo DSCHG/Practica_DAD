6aea147ccc4048c5deb408b835675ed39b42e335
eva <eva.1987@gmail.com>
2020-02-17 22:00:44 +0100

    busqueda index

--- Practica/Practica/src/main/java/com/practica/toko/ControllerServicio1.java
+++ Practica/Practica/src/main/java/com/practica/toko/ControllerServicio1.java
@@ -84,24 +84,15 @@ public class ControllerServicio1 {
 	}
 	
 	@RequestMapping("/formbusqueda")
-	public String mostrarbusquedas(Model model, String search) {
-		
-		List<Producto> listaproductos = productos.findAll();
-		model.addAttribute("producto", listaproductos);
-		for (Producto u : listaproductos) {
-			System.out.println(search);
-			
-			if(search==null) {
+	public String mostrarbusquedas(Model model,@RequestParam (value= "search") String search) {
+
+			List<Producto> listaproductos = productos.findByNombre(search);
+			model.addAttribute("producto", listaproductos);
+			for (Producto u : listaproductos) {
 				model.addAttribute("id", u.getId());
 				model.addAttribute("nombre", u.getNombre());
 				model.addAttribute("precio", u.getPrecio());
 			}
-			else if(search.equals(u.getNombre())) {
-				model.addAttribute("id", u.getId());
-				model.addAttribute("nombre", u.getNombre());
-				model.addAttribute("precio", u.getPrecio());
-			}
-		}
 		return "crudbusqueda";
 	}
 
--- Practica/Practica/src/main/java/com/practica/toko/repositorios/ProductoRepository.java
+++ Practica/Practica/src/main/java/com/practica/toko/repositorios/ProductoRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.practica.toko.model.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+
+	List<Producto> findByNombre(String nombre);
 	//@query("Select * from Producto,Proveedor where proveedor_id=id")
+
 }
