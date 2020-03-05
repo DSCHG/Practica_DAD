5a4d710b750da3bf4a744b5e873bf734c0d94579
Darwin <ds.chumapi.2017@alumnos.urjc.es>
2020-03-05 11:25:49 +0100

    delete change of Eva

--- Practica/Practica/src/main/java/com/practica/toko/repositorios/CarroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practica.toko.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practica.toko.model.Carro;
-
-public interface CarroRepository extends JpaRepository<Carro, Integer> {
-
-}
--- Practica/Practica/src/main/java/com/practica/toko/repositorios/ProveedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practica.toko.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practica.toko.model.Proveedor;
-
-public interface ProveedorRepository extends JpaRepository<Proveedor, Integer>{
-
-}
